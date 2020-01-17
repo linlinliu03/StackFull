aed18ef06d1411dd627791193ccc1ed3273bf809
linlinliu <linlinliu03@gmail.com>
2020-01-16 23:54:44 -0800

    fix answer vote bug

--- app/controllers/api/downvotes_controller.rb
+++ app/controllers/api/downvotes_controller.rb
@@ -1,26 +1,12 @@
 class Api::DownvotesController < ApplicationController
     def create 
-        downvote = Downvote.find_by(user_id:current_user.id, answer_id:params[:downvote][:answer_id])
-        if downvote
-            @downvote = downvote
-            @downvote.destroy
-            render :show
+        @downvote = Downvote.create(downvote_params)
+        @downvote.user_id = current_user.id
+        if @downvote.save 
+          render :show
         else  
-            @downvote = Downvote.create(downvote_params)
-            @downvote.user_id = current_user.id
-              if @downvote.save 
-                render :show
-              else  
-                render json: @downvote.errors.full_messages, status: 422
-              end 
+          render json: @downvote.errors.full_messages, status: 422
         end 
-        # @downvote = Downvote.create(downvote_params)
-        # @downvote.user_id = current_user.id 
-        # if @downvote.save
-        #     render :show 
-        # else 
-        #     render json: @downvote.errors.full_messages, status: 422  
-        # end
     end 
 
     def delete
--- app/controllers/api/upvotes_controller.rb
+++ app/controllers/api/upvotes_controller.rb
@@ -1,19 +1,12 @@
 class Api::UpvotesController < ApplicationController
-    def create 
-       upvote = Upvote.find_by(user_id:current_user.id, answer_id:params[:upvote][:answer_id])
-        if upvote
-            @upvote = upvote
-            @upvote.destroy
-            render :show
+    def create     
+        @upvote = Upvote.create(upvote_params)
+        @upvote.user_id = current_user.id
+        if @upvote.save 
+          render :show
         else  
-            @upvote = Upvote.create(upvote_params)
-            @upvote.user_id = current_user.id
-              if @upvote.save 
-                render :show
-              else  
-                render json: @upvote.errors.full_messages, status: 422
-              end 
-        end
+          render json: @upvote.errors.full_messages, status: 422
+        end 
     end  
 
     def delete
